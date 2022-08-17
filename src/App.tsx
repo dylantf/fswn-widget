e14a5bef659363725189cea719a64e3a8c4d9d3b
Dylan Foster <dylantf@gmail.com>
2022-08-16 20:56:47 -0400

    Update arrow. Fix directional names

--- src/App.tsx
+++ src/App.tsx
@@ -52,48 +52,47 @@ export const App: React.FC = () => {
   };
 
   const formatDirection = (deg: number) => {
-    if (deg > 348.75 || deg < 11.25) return "N";
-    if (deg > 11.25 && deg < 33.75) return "NNE";
-    if (deg > 33.75 && deg < 56.25) return "NE";
-    if (deg > 56.25 && deg < 78.75) return "ENE";
-    if (deg > 78.75 && deg < 101.25) return "E";
-    if (deg > 101.25 && deg < 123.75) return "ESE";
-    if (deg > 123.75 && deg < 146.25) return "SE";
-    if (deg > 146.25 && deg < 168.75) return "SSE";
-    if (deg > 168.75 && deg < 191.25) return "S";
-    if (deg > 191.25 && deg < 213.75) return "SSW";
-    if (deg > 213.75 && deg < 236.25) return "SW";
-    if (deg > 236.25 && deg < 258.75) return "WSW";
-    if (deg > 258.75 && deg < 281.25) return "W";
-    if (deg > 281.25 && deg < 303.75) return "WNW";
-    if (deg > 303.75 && deg < 326.25) return "NW";
-    if (deg > 326.25 && deg < 348.75) return "NNW";
+    if (deg >= 348.75 || deg < 11.25) return "N";
+    if (deg >= 11.25 && deg < 33.75) return "NNE";
+    if (deg >= 33.75 && deg < 56.25) return "NE";
+    if (deg >= 56.25 && deg < 78.75) return "ENE";
+    if (deg >= 78.75 && deg < 101.25) return "E";
+    if (deg >= 101.25 && deg < 123.75) return "ESE";
+    if (deg >= 123.75 && deg < 146.25) return "SE";
+    if (deg >= 146.25 && deg < 168.75) return "SSE";
+    if (deg >= 168.75 && deg < 191.25) return "S";
+    if (deg >= 191.25 && deg < 213.75) return "SSW";
+    if (deg >= 213.75 && deg < 236.25) return "SW";
+    if (deg >= 236.25 && deg < 258.75) return "WSW";
+    if (deg >= 258.75 && deg < 281.25) return "W";
+    if (deg >= 281.25 && deg < 303.75) return "WNW";
+    if (deg >= 303.75 && deg < 326.25) return "NW";
+    if (deg >= 326.25 && deg < 348.75) return "NNW";
   };
 
   return (
     <div style={{ textAlign: "center", fontFamily: "sans-serif" }}>
-      <div
+      <img
         title={`${windDir}°`}
+        src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Black_Down_Arrow.png?20091113220103"
         style={{
-          cursor: "default",
-          display: "inline-block",
-          fontSize: "42pt",
+          width: "50px",
+          height: "auto",
           transition: "all 1s ease-in",
           transform: `rotate(${windDir}deg)`,
         }}
-      >
-        ⬇
-      </div>
-      <div style={{ fontSize: "18pt" }}>
+      />
+      <div style={{ fontSize: "18pt", marginTop: "10px" }}>
         <strong title={`${windDir}°`}>{formatDirection(windDir)}</strong>{" "}
         <strong>{curAvg}</strong>
         {curAvg !== curHi && (
           <>
             {" "}
             <span style={{ fontSize: "14pt" }}>to</span>{" "}
-            <strong>{curHi}</strong> knots
+            <strong>{curHi}</strong>
           </>
-        )}
+        )}{" "}
+        knots
       </div>
 
       <div style={{ marginTop: "10px", fontSize: "12pt" }}>
